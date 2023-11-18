b3a8724e354dba32e6e452740c325a0a05d6e586
Phol Castaneda <pholcast17@gmail.com>
2023-11-17 19:28:55 -0500

    GetData completed

--- WebScraping.py
+++ WebScraping.py
@@ -2,6 +2,7 @@ from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import json
 from datetime import datetime
+import random
 
 def getBookLinks():
 
@@ -32,9 +33,17 @@ def convertDate(dateString):
      date_without_prefix = dateString.split(' ', 2)[-1]
 
      # Convert the remaining date to the desired format
-     formatted_date = datetime.strptime(date_without_prefix, '%B %d, %Y').strftime('%d/%m/%Y')
+     try:
+      formatted_date = datetime.strptime(date_without_prefix, '%B %d, %Y').strftime('%d/%m/%Y')
+     except:
+      months = ['January', 'February', 'March', 'April', 'May', 'June',
+                'July', 'August', 'September', 'October', 'November', 'December']
+      formatted_date= date_without_prefix.replace(",","")
+      formatted_date = formatted_date.split(" ")
 
-     return formatted_date  # Output: 11/07/1960
+      formatted_date = f"{formatted_date[1]}/{str(months.index(formatted_date[0])+1)}/{formatted_date[2]}"
+
+     return formatted_date
 
 
 def getData(dictionary, link):
@@ -43,8 +52,14 @@ def getData(dictionary, link):
         objetoBS = BeautifulSoup(html.read(), features="html.parser")
         
         
+        try:
+            title = objetoBS.find("h1",{"class":"Text Text__title1"}).get_text()
+        except Exception as e:
+            print("Hubo una tragedia:",e)
+            return dictionary
 
-        title = objetoBS.find("h1",{"class":"Text Text__title1"}).get_text()
+
+
 
 
         author = objetoBS.find("span",{"class":"ContributorLink__name"}).get_text()
@@ -53,23 +68,42 @@ def getData(dictionary, link):
         date = objetoBS.find("p",{"data-testid":"publicationInfo"}).get_text()
         date = convertDate(date)
 
-        genresSpan = objetoBS.find_all("span",{"class":"BookPageMetadataSection__genreButton"})
+        genresSpan = objetoBS.find_all("span",{"class":"BookPageMetadataSection__genreButton"}) 
 
         genresList = []
-
-        for i in range(3):
+        
+        for i in range(len(genresSpan)):
+             if i == 3:
+                 break
              genre = genresSpan[i].find("span").get_text()
              genresList.append(genre)
+             
+        
+        button = objetoBS.find("button",{"class":"Button Button--buy Button--medium Button--block"})
 
+        spanPrice = button.find("span").get_text()
+
+        price = round(random.uniform(0.50,20.99),2)
+        
+        if "$" in spanPrice:
+            price = float(spanPrice.split("$")[-1])
+        
+
+            
+            
+
+
+        
+        print("Precio: ",price)
         print("Generos: ",genresList)
         print("Fecha: ",date)
         print("Titulo: ",title)
         print("Autor: ",author)
         print("Calificacion: ",rating)
 
-        data[title] = {"Autor": author,"Fecha Publicacion":date,"Precio":None,"Calificacion":rating, "Generos":genresList}
+        dictionary[title] = {"Autor": author,"Fecha Publicacion":date,"Precio":price,"Calificacion":rating, "Generos":genresList}
 
-        return data
+        return dictionary
         
 
 
